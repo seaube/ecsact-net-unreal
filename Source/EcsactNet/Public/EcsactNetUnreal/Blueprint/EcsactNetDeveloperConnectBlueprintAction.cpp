d1ccabb89947d3acc0176a7b69a5da190827b0dd
Ezekiel Warren <ezekiel@seaube.com>
2024-12-14 10:30:14 -0800

    fix: add error if developer connect is used outside of PIE

--- Source/EcsactNet/Public/EcsactNetUnreal/Blueprint/EcsactNetDeveloperConnectBlueprintAction.cpp
+++ Source/EcsactNet/Public/EcsactNetUnreal/Blueprint/EcsactNetDeveloperConnectBlueprintAction.cpp
@@ -1,4 +1,5 @@
 #include "EcsactNetDeveloperConnectBlueprintAction.h"
+#include "Engine/World.h"
 #if WITH_EDITOR
 #	include "EcsactNetEditor/EcsactNetSettings.h"
 #	include "EcsactNetEditor/EcsactNetEditor.h"
@@ -14,19 +15,29 @@ auto UEcsactNetDeveloperConnectBlueprintAction::DeveloperConnect(
 
 auto UEcsactNetDeveloperConnectBlueprintAction::Activate() -> void {
 #if WITH_EDITOR
-	const auto* settings = GetDefault<UEcsactNetSettings>();
-	const auto  NodeId = settings->NodeId;
+	if(!IsRunningGame()) {
+		const auto* settings = GetDefault<UEcsactNetSettings>();
+		const auto  NodeId = settings->NodeId;
 
-	auto req = FNodeAuthRequest{.nodeId = NodeId, .address = "0.0.0.0"};
-	FEcsactNetEditorModule::Get().GetHttpClient()->NodeAuth(
-		req,
-		TDelegate<void(FNodeAuthResponse)>::CreateLambda( //
-			[this](FNodeAuthResponse response) {
-				ConnectRequest(TCHAR_TO_UTF8(*response.nodeConnectionUri));
-			}
-		)
-	);
-
+		auto req = FNodeAuthRequest{.nodeId = NodeId, .address = "0.0.0.0"};
+		FEcsactNetEditorModule::Get().GetHttpClient()->NodeAuth(
+			req,
+			TDelegate<void(FNodeAuthResponse)>::CreateLambda( //
+				[this](FNodeAuthResponse response) {
+					ConnectRequest(TCHAR_TO_UTF8(*response.nodeConnectionUri));
+				}
+			)
+		);
+	} else {
+		// TODO: if we move the http client to a 'developer' module we should be
+		// able to connect here as well
+		UE_LOG(
+			LogTemp,
+			Error,
+			TEXT("EcsactNet DeveloperConnect is only available while playing in "
+					 "editor")
+		);
+	}
 #else
 	UE_LOG(
 		LogTemp,
